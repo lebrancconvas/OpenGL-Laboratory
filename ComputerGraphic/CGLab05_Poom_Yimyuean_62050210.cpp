e02298d1022a5d73d8d9670fe781603a2bdc5119
lebrancconvas <lebrancconvas@gmail.com>
2021-02-18 20:11:59 +0700

    CGLab 04

---
+++ ComputerGraphic/CGLab05_Poom_Yimyuean_62050210.cpp
@@ -0,0 +1,19 @@
+//
+//  CGLab05_Poom_Yimyuean_62050210.cpp
+//  ComputerGraphic
+//
+//  Created by Poom Yimyuean on 18/2/2564 BE.
+//
+
+#include <iostream>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main() {
+    cout << "Unreal Engine 4 is make my macbook hot.\n";
+}
